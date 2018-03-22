934366b42039b2b1ef5017b466c4440eab32130b
Carlos Augusto Aragón Cruz <ing.c.aragon@gmail.com>
2018-03-22 02:06:40 -0600

    Add findByName method

--- src/main/java/hello/repository/ClienteRepository.java
+++ src/main/java/hello/repository/ClienteRepository.java
@@ -1,7 +1,11 @@
-package hello;
+package hello.repository;
 
+import hello.domain.Cliente;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
 
+    List<Cliente> findByName(String name);
 }
